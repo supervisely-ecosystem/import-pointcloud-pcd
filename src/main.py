f24046301751a86edf6dfb72ed7dc25774482d6e
peixed351 <peixed351@gmail.com>
2023-02-05 19:02:03 +0300

    in progress

--- src/main.py
+++ src/main.py
@@ -83,8 +83,7 @@ class MyImport(sly.app.Import):
             source_dir_name = context.path.lstrip("/").rstrip("/")
             sly.logger.info(msg=f"Source directory: '{source_dir_name}' was successfully removed.")
 
-        f.shutdown_app()
-
 
 app = MyImport()
 app.run()
+f.shutdown_app()
