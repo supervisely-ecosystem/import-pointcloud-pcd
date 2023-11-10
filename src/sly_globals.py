0753b254b5dc3411f6824bb33f3aad961629eed7
almaz <almazgimaev@gmail.com>
2023-11-10 12:35:55 +0100

    fix type

--- src/sly_globals.py
+++ src/sly_globals.py
@@ -9,7 +9,7 @@ if sly.is_development():
     load_dotenv("local.env")
     load_dotenv(os.path.expanduser("~/supervisely.env"))
 
-api: sly.Api = sly.Api.from_env()
+api = sly.Api.from_env()
 
 TASK_ID = int(os.environ["TASK_ID"])
 TEAM_ID = int(os.environ["context.teamId"])
